9cf41bf0f0a7276994dca6195f8187404a59630d
faii <pooeifaii@gmail.com>
2014-05-17 12:41:54 +0700

    New index

--- index.php
+++ index.php
@@ -3,10 +3,40 @@
 <head>
 <meta charset="utf-8">
 <title>ผ้าขาวม้า</title>
+<style type="text/css">
+a:link {
+	color: #C00;
+	text-decoration: none;
+}
+a:visited {
+	text-decoration: none;
+	color: #C00;
+}
+a:hover {
+	text-decoration: none;
+	color: #C90;
+}
+a:active {
+	text-decoration: none;
+	color: #900;
+}
+</style>
 </head>
 
-<body>
-<p>เมนู</p>
-<p><a href="voice_list.php">รายชื่อนักพากษ์</a></p>
+<body><center>
+<table width="500" border="0">
+  <tr>
+    <td colspan="2" align="center" bgcolor="#DEDEDE"><img src="voice_over.jpg" width="60" height="91"></td>
+  </tr>
+  <tr>
+    <td width="135" valign="top"><b><i>เมนู</i></b><li><a href="voice_list.php">นักพากษ์</a></li>
+      <li>บอร์ด</li></td>
+    <td width="355">&nbsp;</td>
+  </tr>
+  <tr>
+    <td colspan="2" align="center" bgcolor="#DEDEDE">Pa Kauw Ma</td>
+  </tr>
+</table>
+<p>&nbsp;</p></center>
 </body>
 </html>
