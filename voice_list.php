385146a0cbbafa4eab707908c9afc4fb1b97cbb8
faii <pooeifaii@gmail.com>
2014-05-17 14:48:00 +0700

    Add new file

--- voice_list.php
+++ voice_list.php
@@ -10,7 +10,7 @@ include("connect.php");
 <title>ผ้าขาวม้า</title>
 </head>
 
-<body>
+<body><center><form id="form1" name="form1" method="post" action="">
 
        <?php
 		$sqle="";
@@ -18,6 +18,8 @@ include("connect.php");
 		$chk=0;
 				$sqle="SELECT * FROM voiceovers";
 				$rse=$cdb->getsql($sqle);
+
+	
 				if(count($rse)>=1){$chk=1;} 
 				
 			else{$chk=2;}
@@ -31,29 +33,36 @@ include("connect.php");
 			?>
          </font></p>
      <font size="2" color="#333333">
-<table  accesskey="1" cellpadding="1" cellspacing="1" bordercolor="#0072A6">
-  <tr bgcolor="#006EA6">
-
-    <td width="49"><div align="center" class="style1">รหัส</div></td>
-      <td width="153"><div align="center" class="style1">ชื่อ-สกุล</div></td>
-      <td width="80"><div align="center" class="style1">ชื่อเล่น</div></td>
-	 <td width="80"><div align="center" class="style1">รูป</div></td>
-    </tr>
+<table  accesskey="1" cellpadding="1" cellspacing="1" >
+  <tr>
+    <td height="41" colspan="3" align="center" valign="top" bgcolor="#DEDEDE"><img src="voice_over.jpg" width="59" height="91" /></td>
+  </tr>
   <?php
 	for($i=0;$i<$nc;$i++)
 	{
 	 ?>
   <tr>
-    <td width="49" height="41" align="center" valign="top"><?php echo($rse[$i]['voic_id']);?></td>
-      <td width="153" align="left" valign="top"><?php echo($rse[$i]['voic_fname']);?>&nbsp;&nbsp;<?php echo($rse[$i]['voic_lname']);?></td>
-      
-      <td align="left" valign="top"><?php echo($rse[$i]['voic_nick']);?></td>
-	  <td align="left" valign="top"><img src=<?php echo($rse[$i]['voic_photo']);?> ></td>
-    </tr>
+    <td width="100" height="41" rowspan="2" align="center" valign="top"><img src=<?php echo($rse[$i]['voic_photo']);?> height="100" /><br /><br /></td>
+    <td width="67" rowspan="2" align="right" valign="top"><span class="style1">ชื่อ-สกุล</span> : <br />
+    <span class="style1">ชื่อเล่น</span> : </td>
+    <td width="166" align="left" valign="top"><?php echo($rse[$i]['voic_fname']);?>&nbsp;&nbsp;<?php echo($rse[$i]['voic_lname']);?><br />
+    <?php echo($rse[$i]['voic_nick']);?><br /></td>
+  </tr>
+  <tr>
+    <td align="right" valign="top"><font color="#333333" size="2">
+    <a href="profile.php?edit=1&id=<?php echo($rse[$i]['voic_id']);?>">เพิ่มเติม</a></font>
+      <p>&nbsp;</p>
+      <p>&nbsp;</p></td>
+  </tr>
+
   <?php }//end for ?>
+    <tr>
+    <td height="41" colspan="3" align="center" valign="top" bgcolor="#DEDEDE"><p>
+    </p>  </td>
+    </tr>
 </table>
 </font>
-     <p>&nbsp;</p>
+<p>&nbsp;</p>
      <p><a href="index.php">กลับ</a><font size="2" color="#333333">
        <?php
 		
@@ -66,7 +75,7 @@ elseif($chk==2)
  ?>
        
        </font>
-     </p>
-  <p>
+     </p></form>
+  </center>
 </body>
 </html>
