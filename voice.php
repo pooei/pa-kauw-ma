4c266393f008876dd55037008688f06f2c883f83
faii <pooeifaii@gmail.com>
2014-05-17 10:52:19 +0700

    Add VoiceProfile design file

---
+++ voice.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<style type="text/css">
+body,td,th {
+	font-family: Tahoma, Geneva, sans-serif;
+	font-size: 14px;
+}
+</style>
+</head>
+
+<body>
+<table width="300" border="1">
+  <tr>
+    <td colspan="3" align="center">ประวัตินักพากษ์</td>
+  </tr>
+  <tr>
+    <td width="121" rowspan="6" valign="top">&nbsp;</td>
+    <td width="63" align="right" valign="top">ชื่อ : </td>
+    <td width="94" valign="top">&nbsp;</td>
+  </tr>
+  <tr>
+    <td align="right" valign="top">นามสกุล : </td>
+    <td valign="top">&nbsp;</td>
+  </tr>
+  <tr>
+    <td align="right" valign="top">ชื่อเล่น : </td>
+    <td valign="top">&nbsp;</td>
+  </tr>
+  <tr>
+    <td align="right" valign="top">วันเกิด : </td>
+    <td valign="top">&nbsp;</td>
+  </tr>
+  <tr>
+    <td align="right" valign="top">E-mail : </td>
+    <td valign="top">&nbsp;</td>
+  </tr>
+  <tr>
+    <td align="right" valign="top">คติ : </td>
+    <td valign="top">&nbsp;</td>
+  </tr>
+  <tr>
+    <td align="right" valign="top">ประวัติ : </td>
+    <td colspan="2" align="right" valign="top">&nbsp;</td>
+  </tr>
+</table>
+</body>
+</html>
